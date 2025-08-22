51f7c9601d9c8e661e7ceb20d7b7ab24bad49fe3
Serhii-Leonenko <leonenko3149@gmail.com>
2025-08-22 11:04:16 +0300

    dropbox upgrade

--- base/storages.py
+++ base/storages.py
@@ -6,6 +6,8 @@ from storages.utils import safe_join
 
 
 class WindowsCompatibleDropboxStorage(DropboxStorage):
+    CACHE_TTL = 60 * 60 * 4 - 10
+
     def _full_path(self, name):
         if name == "/":
             name = ""
@@ -15,3 +17,29 @@ class WindowsCompatibleDropboxStorage(DropboxStorage):
             return os.path.join("/", self.root_path, name).replace("\\", "/")
         else:
             return safe_join(self.root_path, name).replace("\\", "/")
+
+    # @staticmethod
+    # def _get_cache_key(name: str) -> str:
+    #     return f"dropbox:media:{name}"
+    #
+    # def url(self, name):
+    #     cache_key = self._get_cache_key(name)
+    #
+    #     if link := cache.get(cache_key):
+    #         return link
+    #
+    #     link = super().url(name)
+    #     cache.set(cache_key, link, self.CACHE_TTL)
+    #
+    #     return link
+
+    def _save(self, name, content):
+        name = super()._save(name, content)
+
+        link = self.client.sharing_create_shared_link(self._full_path(name))
+        url = link.url.replace("dl=0", "dl=1")
+
+        return url
+
+    def url(self, name):
+        return name
